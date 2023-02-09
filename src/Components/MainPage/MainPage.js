ae7d9f1875f02380bbfaf363b2a6be8ad490c6a7
WeslleiBrito <wesllei.wbs@gmail.com>
2023-02-08 21:21:46 -0300

    exercicio finalizado

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,20 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {MainContainer, Form, Input} from './styles'
 
 function MainPage() {
+
+  const [inputName, setInputName] = useState("")
+  const [inputAge, setInputAge] = useState("")
+  const [inputEmail, setInputEmail] = useState("")
+  const [inputPassword, setPassword] = useState("")
+
+  function btnEnviar (){
+    console.log(`Name: ${inputName}\nAge: ${inputAge}\nEmail: ${inputEmail}\nPassword: ${inputPassword}`)
+    clearEntry()
+  }
+
+  function clearEntry(){
+    setInputName("")
+    setInputAge("")
+    setInputEmail("")
+    setPassword("")
+  }
+  function modifyInputName(event){
+    setInputName(event.target.value)
+  }
+
+  function modifyInputAge(event){
+    setInputAge(Number(event.target.value))
+  }
+
+  function modifyInputEmail(event){
+    setInputEmail(event.target.value)
+  }
+
+  function modifyInputPassword(event){
+    setPassword(event.target.value)
+  }
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       <Form>
         <label>
           Nome:
-          <Input />
+          <Input value={inputName} onChange={modifyInputName}/>
         </label>
         <label>
           Idade:
-          <Input/>
+          <Input value={inputAge} onChange={modifyInputAge}/>
         </label>  
-        <button>Enviar dados</button>
+        <label>
+          Email:
+        </label>
+        <Input type="email" value={inputEmail} onChange={modifyInputEmail}/>
+        <label>
+          Senha:
+        </label>
+        <Input type="password" value={inputPassword} onChange={modifyInputPassword}/>
+     
+        <button onClick={btnEnviar}>Enviar dados</button>
       </Form>
     </MainContainer>
   )
